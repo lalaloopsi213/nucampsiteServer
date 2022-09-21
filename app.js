9170487921439144c6cd270aa77c35297be1d1e4
lalaloopsi213 <fg1041@gmail.com>
2022-09-21 10:55:01 -0500

    Express Session Part 2

--- app.js
+++ app.js
@@ -46,6 +46,17 @@ app.use(session({
   resave: false,
   store: new FileStore()
 }));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+function auth(req, res, next) {
+    console.log(req.session);
+
+    if (!req.session.user) {
+        const err = new Error('You are not authenticated!');
+        err.status = 401;
+        return next(err);
+    } 
 
 function auth(req, res, next) {
   console.log(req.session);
@@ -54,18 +65,9 @@ function auth(req, res, next) {
       const authHeader = req.headers.authorization;
       if (!authHeader) {
           const err = new Error('You are not authenticated!');
-          res.setHeader('WWW-Authenticate', 'Basic');
           err.status = 401;
           return next(err);
-      }
-
-      const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-      const user = auth[0];
-      const pass = auth[1];
-      if (user === 'admin' && pass === 'password') {
-          req.session.user = 'admin';
-          return next(); // authorized
-      } else {
+    } else {
           const err = new Error('You are not authenticated!');
           res.setHeader('WWW-Authenticate', 'Basic');
           err.status = 401;
